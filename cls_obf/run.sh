0977ca29d319dbfcd01f65c0a47291906be6a1e6
Shiran <dudy@ohsu.edu>
2017-05-29 15:40:34 -0700

    reload run

--- cls_obf/run.sh
+++ cls_obf/run.sh
@@ -6,7 +6,7 @@
 #This shell script's purpose is to train class classifier for obfuscation encoding detection.
 
 # Determine paths for folders
-txt2obfuscate='/Users/dudy/CSLU/4th_year/term41/obf_detection/enwik8'
+txt2obfuscate=TBD
 path2db='db'
 path2folds='db/folders'
 path2models='models'
@@ -27,23 +27,23 @@ python3 synthesize.py $train_seed $txt2obfuscate > $path2db/train_data
 echo --------------------------------------
 echo Generate Synthetic Data for validation
 echo --------------------------------------
-#python3 synthesize.py $valid_seed $txt2obfuscate > $path2db/valid_data
+python3 synthesize.py $valid_seed $txt2obfuscate > $path2db/valid_data
 
 # split to five folds
 echo ----------------
 echo Five Folds Split
 echo ----------------
-#python split_5.py $path2db/train_data $path2folds train
-#python split_5.py $path2db/valid_data $path2folds valid
+python split_5.py $path2db/train_data $path2folds train
+python split_5.py $path2db/valid_data $path2folds valid
 
 # train
 echo -----
 echo Train
 echo -----
-#python train.py $train_seed $path2folds $path2models
+python train.py $train_seed $path2folds $path2models
 
 # evaluate
 echo --------
 echo Evaluate
 echo --------
-#python evaluate.py $path2folds $path2models
+python evaluate.py $path2folds $path2models
